d9a6ce84a7434adc8ad3e02d1219f1621eaa1ac1
Veronica-Alfr <veagalves@gmail.com>
2021-11-26 18:31:28 -0300

    Arq script.js criado

---
+++ 01-fundamentos/bloco-04-intro-js-logica/script.js
@@ -0,0 +1,17 @@
+let age = 21;
+let fullName = "Verônica Alves";
+let city = "Montes Claros";
+
+console.log(age);
+console.log(fullName);
+console.log(city)
+
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //cor nula para ser atribuida seu valor dps
+
+// o operador ++ incrementa de 1 a  1, e contrário com --
+
+num1+=num2 //soma e atribui o resultado em num1, sendo colocado no console.log(num1)
+
+//console.log(idade >=18 ? "Pode dirigir" : "Não pode"); //if ternário
