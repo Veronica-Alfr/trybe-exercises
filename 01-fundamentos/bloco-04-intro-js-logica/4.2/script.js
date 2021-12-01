a205aff66649c70746faf2a92c8d2bb26ad20c2b
Veronica-Alfr <veagalves@gmail.com>
2021-12-01 00:24:00 -0300

    Atv 4.2 finalizada

--- 01-fundamentos/bloco-04-intro-js-logica/4.2/script.js
+++ 01-fundamentos/bloco-04-intro-js-logica/4.2/script.js
@@ -21,9 +21,9 @@ arredondar: math.trunc, parseInt, round, floor, tofixed(0).
 array dentro de outro array (2 camadas é matriz)
 */
 
-/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let soma = 0
+/*let soma = 0
 for (index = 0; index < numbers.length; index++) {
     soma += (numbers[index])   
 } 
@@ -40,27 +40,33 @@ let maior = 0
 for(index = 0; index < numbers.length; index++) {
       if(numbers[index] > maior)
       maior = numbers[index]
-} console.log(maior)
+} console.log(maior) */
 
 //imprir a quantidade de números impares
-cont = 0
-for (index = 0; index < numbers.length; index++) {
-    if (numbers[index] % 2 >= 1)
-     console.log()
-} */
+
+/*let impar = [];
+// a varia i e index é entendível pelo JS como ínicio, não precisando de declaração duma var/let.
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] % 2 == 1) {
+      impar.push(numbers[index])
+        
+    }
+} console.log(impar.length)*/
 
 //menor valor imprimido
 
 
+
 // imprimir array de 1 à 25, e retonar a /2 de cada
 let array = []
-let divisao = []
+
 for (i = 1; i < 26; i++) {
     array.push(i)
-for (i = 0; i < array.length; i++) {
-
-}
 }console.log(array)
-console.log(divisao)
+
+for (j = 0; j < array.length; j++) {
+    console.log(array[j]/2)
+}
+
 
 
