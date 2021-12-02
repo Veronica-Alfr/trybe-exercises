f80c6eada4df6fc083816f6853e4aa8fb0921160
Veronica-Alfr <veagalves@gmail.com>
2021-12-01 22:39:18 -0300

    Atv 4.4 livros em processo

--- 01-fundamentos/bloco-04-intro-js-logica/4.4/livros.js
+++ 01-fundamentos/bloco-04-intro-js-logica/4.4/livros.js
@@ -10,3 +10,8 @@ let leitor = {
       },
     ],
   };
+  leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  })
