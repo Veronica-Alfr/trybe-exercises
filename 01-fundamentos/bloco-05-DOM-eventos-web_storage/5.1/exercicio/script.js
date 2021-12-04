d4593d605496b9e3609d0ccbc998a07b79705b7b
Veronica-Alfr <veagalves@gmail.com>
2021-12-04 01:09:15 -0300

    Exercício 5.1 em progresso

---
+++ 01-fundamentos/bloco-05-DOM-eventos-web_storage/5.1/exercicio/script.js
@@ -0,0 +1,11 @@
+function paragrafo(frase) {
+    document.getElementsByTagName("p")[1].innerText = frase
+}
+paragrafo("Me vejo numa empresa internacional.");
+
+document.querySelector(".main-content").style.backgroundColor = "rgb(76,164,109)";
+
+document.querySelector(".center-content").style.backgroundColor = "white";
+
+document.querySelector(".title").innerText = "Exercício 5.1 - JavaScript";
+
