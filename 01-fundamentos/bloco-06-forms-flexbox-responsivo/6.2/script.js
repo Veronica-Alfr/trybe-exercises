bf462c73d74088f82c79a72d1bb9f67de57e0846
Veronica-Alfr <veagalves@gmail.com>
2022-01-06 13:08:15 -0300

    Arq removido

--- 01-fundamentos/bloco-06-forms-flexbox-responsivo/6.2/script.js
+++
@@ -1,8 +0,0 @@
-/*let moment = require("moment");
-let frowNow = brTetra.fromNow();
-
-console.log("Brasil é treta" + frowNow);
-*/
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
-    alert('Voilà!');
-  });
