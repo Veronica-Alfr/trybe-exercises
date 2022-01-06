2cba033b4b8ba2fa93eacfb2610f6ffea84eb162
Veronica-Alfr <veagalves@gmail.com>
2022-01-05 21:06:31 -0300

    Atv 6.1 em progresso no requisito 3

--- 01-fundamentos/bloco-06-forms-flexbox-responsivo/6.1/script.js
+++ 01-fundamentos/bloco-06-forms-flexbox-responsivo/6.1/script.js
@@ -1,6 +1,7 @@
 let states = document.getElementById("states");
 let statesBrazil =
-   [ 
+   [
+    "",
     "Acre",
     "Alagoas",
     "Amapá",
@@ -73,3 +74,74 @@ function datesFunc(event) {
  dates.addEventListener("keypress", datesFunc);
  dates.addEventListener("change", datesVal);
 
+let btn = document.getElementById("btn");
+btn.addEventListener("click", buttonSubmit)
+let inputName = document.getElementById("names");
+let inputEmail = document.getElementById("emails");
+let inputCpf = document.getElementById("cpfs");
+let inputEnd = document.getElementById("end");
+let inputCity = document.getElementById("citys");
+let selectState = document.getElementById("states");
+let inputHome = document.getElementById("residencia-1");
+let inputApt = document.getElementById("residencia-2");
+let inputCargo = document.getElementById("cargo-atual");
+let textArea = document.getElementById("resumo-curriculo");
+let inputDescricao = document.getElementById("cargo-descricao");
+
+function buttonSubmit (event) {
+if (inputName.value.length > 40) {
+  event.preventDefault();
+  alert("Quantidade de caracteres inválidos!");
+}
+
+  if (inputEmail.value.length > 50) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (inputCpf.value.length > 11) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (inputEnd.value.length > 200) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (inputCpf.value.length > 11) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (inputCity.value.length > 28) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (selectState.value == "") {
+    event.preventDefault();
+    alert("Escolha um estado!");
+  }
+
+  if (inputCargo.value.length > 40) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (inputDescricao.value.length > 500) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (textArea.value.length > 1000) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+
+  if (textArea.value.length > 1000) {
+    event.preventDefault();
+    alert("Quantidade de caracteres inválidos!");
+  }
+  
+}
