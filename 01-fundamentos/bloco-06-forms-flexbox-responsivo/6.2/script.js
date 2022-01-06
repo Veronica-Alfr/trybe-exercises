24015eabd90ab3c9df977772401312b804255be0
Veronica-Alfr <veagalves@gmail.com>
2022-01-06 13:12:54 -0300

    Arq add

---
+++ 01-fundamentos/bloco-06-forms-flexbox-responsivo/6.2/script.js
@@ -0,0 +1,8 @@
+/*let moment = require("moment");
+let frowNow = brTetra.fromNow();
+
+console.log("Brasil é treta" + frowNow);
+*/
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+    alert('Voilà!');
+  });
