c3e27ce5e698dfd21010e192880cf1638185ffa8
Veronica-Alfr <veagalves@gmail.com>
2022-01-18 23:55:46 -0300

    Exc 3 de 8.1 99% finalizado

---
+++ 01-fundamentos/bloco-08-HOF/8.1/exc3.js
@@ -0,0 +1,26 @@
+checarCalcular = (arrayGabarito, arrayRespostas) => {
+    let count = 0;
+      for (let i = 0; i < arrayGabarito.length; i += 1) {
+        for (let x = i; x < arrayRespostas.length; x += 1) {
+            if (arrayGabarito[i] === arrayRespostas[x]) {
+              count += 1;
+              break; 
+        } else if (arrayRespostas[x] === 'N.A') {
+            break;
+        }
+         else {  
+            count -= 0.5;
+            break;
+        }
+        }
+      }
+      return count;
+    }
+
+const hof = (gabarito, respostas, func) => {
+    return func(gabarito, respostas)
+}
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+console.log();
