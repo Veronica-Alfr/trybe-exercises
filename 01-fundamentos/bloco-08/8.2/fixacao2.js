4ede3801061474bfb623f70f54b814e22df38361
Veronica-Alfr <veagalves@gmail.com>
2022-01-20 12:42:42 -0300

    fixação concluída, exc3 em progresso

--- 01-fundamentos/bloco-08/8.2/fixacao2.js
+++ 01-fundamentos/bloco-08/8.2/fixacao2.js
@@ -8,3 +8,13 @@ const findNameWithFiveLetters = names.find((name) => name.length === 5);
 // nessa função eu 'falo' que o comprimento de name(que é cada elemento da const names) é 5
 
 console.log((findNameWithFiveLetters));
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  const findMusic = (id) =>{ const finded = musicas.find((obj) => obj.id === id)
+  return finded.title;
+  }
+  console.log(findMusic('31031685'))
