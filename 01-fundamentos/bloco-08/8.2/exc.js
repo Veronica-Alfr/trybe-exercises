78d4121236a6581f7963224f3f8ba6e54a6ed638
Veronica-Alfr <veagalves@gmail.com>
2022-01-21 00:09:48 -0300

    Exc 8.2 finalizado

--- 01-fundamentos/bloco-08/8.2/exc.js
+++ 01-fundamentos/bloco-08/8.2/exc.js
@@ -61,25 +61,55 @@ const books = [
     },
   ];
   
-  function authorBornIn1947() {
-    // escreva aqui o seu código
-    const book  = books.find((obj) => obj.author.birthYear === 1947)
-    return book.author.name;
-  }
-  console.log(authorBornIn1947());
+    function authorBornIn1947() {
+      // escreva aqui o seu código
+      const book  = books.find((obj) => obj.author.birthYear === 1947)
+      return book.author.name;
+    }
+    console.log(authorBornIn1947());
 
-  function smallerName() {
-    let nameBook = books[0].name;
-    // escreva aqui o seu código
-  books.forEach((book) => {
-      if(book.name.length < nameBook.length) {
-          nameBook = book.name;
+    function smallerName() {
+      let nameBook = books[0].name;
+      // escreva aqui o seu código
+    books.forEach((book) => {
+        if(book.name.length < nameBook.length) {
+            nameBook = book.name;
+        }
+    })
+      // Variável nameBook que receberá o valor do menor nome;
+      return nameBook;
+    }
+    console.log(smallerName());
+
+    function getNamedBook() {
+      // escreva seu código aqui
+      const names = books.find((obj) => obj.name.length === 26)
+      return names.name
+    }
+    console.log(getNamedBook());
+
+    const booksOrder = () => books.sort((obj1, obj2) => obj2.releaseYear - obj1.releaseYear);
+    console.log(booksOrder());
+
+  function everyoneWasBornOnSecXX() {
+    return books.every((el) => el.author.birthYear > 1900 && el.author.birthYear <= 2000);
+  }
+  console.log(everyoneWasBornOnSecXX());
+
+  function someBookWasReleaseOnThe80s() {
+    // escreva seu código aqui
+    return books.some((el) => el.releaseYear > 1980 && el.releaseYear <= 1989);
+  }
+  console.log(someBookWasReleaseOnThe80s());
+
+  function authorUnique() {
+    // escreva seu código aqui
+    //caso nenhum tenha nascido no mesmo ano retorne true
+    return books.every((nasceu) => books.some((nasceuMesmo) => {
+      const result = nasceu.birthYear === nasceuMesmo.birthYear
+      if(result) {
+        return false
       }
-  })
-    // Variável nameBook que receberá o valor do menor nome;
-    return nameBook;
+    }))
   }
-  console.log(smallerName())
-
-
-
+  console.log(authorUnique());
--- 01-fundamentos/bloco-08/8.2/fixacao3.js
+++ 01-fundamentos/bloco-08/8.2/fixacao3.js
@@ -1,32 +1,32 @@
-//1 - Escreva uma função que dado um array de nomes e um nome
-//retorne true se ele estiver contido e caso contrário, 
-//retorne false , use some ;
+// 1 - Escreva uma função que dado um array de nomes e um nome
+// retorne true se ele estiver contido e caso contrário, 
+// retorne false , use some ;
 
-// const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-// const hasName = (arr, name) => {
-//   return arr.some((nameContem) => nameContem === name);
-// }
+const hasName = (arr, name) => {
+  return arr.some((nameContem) => nameContem === name);
+}
 
-// console.log(hasName(names, 'Ana'));
+console.log(hasName(names, 'Ana'));
 
-//2-Escreva uma função que dado um array de pessoas e uma idade mínima 
-//retorne true se todas tiverem a idade maior ou igual a mínima 
-//e caso contrário false , use every ;
-// const people = [
-//   { name: 'Mateus', age: 18 },
-//   { name: 'José', age: 16 },
-//   { name: 'Ana', age: 23 },
-//   { name: 'Cláudia', age: 20 },
-//   { name: 'Bruna', age: 19 },
-// ];
+// 2-Escreva uma função que dado um array de pessoas e uma idade mínima 
+// retorne true se todas tiverem a idade maior ou igual a mínima 
+// e caso contrário false , use every ;
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
 
-// const verifyAges = (arr, minimumAge) => {
-//   //Adicione seu código aqui
-//   return arr.every((obj) => obj.age >= minimumAge);
-// }
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every((obj) => obj.age >= minimumAge);
+}
 
-// console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 18));
 
 const people = [
   { name: 'Mateus', age: 18 },
